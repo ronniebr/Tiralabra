30f8716956f9c184b62cdeaf8b629fa3872cfe71
Ronnie <Ronnie@80.221.8.140>
2012-09-06 16:15:44 +0300

    Testataan josko jo toimis

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ronnie
+ */
+public class Main {
+    //testataanpa
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
